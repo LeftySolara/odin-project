95d1ae3a5e6e9f611fa186728f9f471f02804ff7
Jalen Adams <jalen@jalenkadams.me>
2020-05-01 20:55:03 -0400

    Add: buttons and score counter to rock-paper-scissors

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -7,55 +7,90 @@ function computerPlay() {
     return choices[getRandomNumber(0, 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     let player_choice = playerSelection.toLowerCase();
-    let computer_choice = computerSelection.toLowerCase();
+    let computer_choice = computerPlay().toLowerCase();
+    let result_display = document.querySelector("#result");
+    let player_won = null;
 
     if (player_choice == "rock") {
         if (computer_choice == "paper") {
-            return "You lose! Paper beats rock!"
+            result_display.textContent = "You lose! Paper beats rock!";
+            player_won = false;
         }
         else if (computer_choice == "scissors") {
-            return "You win! Rock beats scissors!";
+            result_display.textContent = "You win! Rock beats scissors!";
+            player_won = true;
         }
         else {
-            return "It's a tie!";
+            result_display.textContent = "It's a tie!";
         }
     }
     else if (player_choice == "paper") {
         if (computer_choice == "rock") {
-            return "You win! Paper beats rock!";
+            result_display.textContent = "You win! Paper beats rock!";
+            player_won = true;
         }
         else if (computer_choice == "scissors") {
-            return "You lose! Scissors beats paper!";
+            result_display.textContent = "You lose! Scissors beats paper!";
+            player_won = false;
         }
         else {
-            return "It's a tie!"
+            result_display.textContent = "It's a tie!"
         }
     }
     else if (player_choice == "scissors") {
         if (computer_choice == "rock") {
-            return "You lose! Rock beats scissors!";
+            result_display.textContent = "You lose! Rock beats scissors!";
+            player_won = false;
         }
         else if (computer_choice == "paper") {
-            return "You win! Scissors beats paper!";
+            result_display.textContent = "You win! Scissors beats paper!";
+            player_won = true;
         }
         else {
-            return "It's a tie!";
+            result_display = "It's a tie!";
         }
     }
-    else {
-        return "Invalid option selected!";
+    
+    let playerScore = document.querySelector("#player-score");
+    let cpuScore = document.querySelector("#cpu-score");
+
+    if (player_won == true) {
+        playerScore.textContent++;
+    }
+    else if (player_won == false) {
+        cpuScore.textContent++;
+    }
+
+    if (playerScore.textContent == 5) {
+        alert("You win!");
+        playerScore.textContent = 0;
+        cpuScore.textContent = 0;
+    }
+    else if (cpuScore.textContent == 5) {
+        alert("You lose!");
+        playerScore.textContent = 0;
+        cpuScore.textContent = 0;
     }
 }
 
-function game() {
-    for (i = 0; i < 5; ++i) {
-        let player_choice = prompt("Rock, paper, or scissors?");
-        let computer_choice = computerPlay();
-        let result = playRound(player_choice, computer_choice);
-        console.log(result);
-    }
+function setupButtons() {
+    let btnRock = document.querySelector("#rock");
+    let btnPaper = document.querySelector("#paper");
+    let btnScissors = document.querySelector("#scissors");
+
+    btnRock.addEventListener("click", function (e) {
+        playRound("rock");
+    });
+
+    btnPaper.addEventListener("click", function (e) {
+        playRound("paper");
+    });
+
+    btnScissors.addEventListener("click", function (e) {
+        playRound("scissors");
+    });
 }
 
-game();
+setupButtons();
