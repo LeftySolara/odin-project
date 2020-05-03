23368f118774945aef5c9e47382fe99122fe62c3
Jalen Adams <jalen@jalenkadams.me>
2020-05-03 10:31:54 -0400

    Add: functionality for changing grid size

--- etch-a-sketch/grid.js
+++ etch-a-sketch/grid.js
@@ -1,3 +1,31 @@
+function createGrid(size) {
+    if (size <= 0) {
+        return;
+    }
+
+    let grid = document.querySelector("#grid");
+    const flexBasis = (size**(-1)) * 100;
+    const flexBasisStr = "calc(" + flexBasis + "%)";
+
+    grid.innerHTML = "";
+    for (let i = 0; i < size**2; ++i) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flexBasis = flexBasisStr;
+
+        square.addEventListener("mouseenter", function (event) {
+            this.style.backgroundColor = "gray";
+        });
+
+        /* Invisible content to help mmaintain aspect ratio */
+        let content = document.createElement("div");
+        content.classList.add("content");
+        square.appendChild(content);
+
+        grid.appendChild(square);
+    }
+}
+
 function clearGrid() {
     const squares = document.querySelectorAll(".square");
 
@@ -6,25 +34,24 @@ function clearGrid() {
     })
 }
 
-function setup() {
-    let grid = document.querySelector("#grid");
-    for (let i = 0; i < 256; ++i) {
-        let square = document.createElement("div");
-        square.classList.add("square");
+function changeGridSize() {
+    const newSize = parseInt(prompt("Enter new grid size", 16), 10);
 
-        square.addEventListener("mouseenter", function (event) {
-            this.style.backgroundColor = "gray";
-        });
-
-        let content = document.createElement("div");
-        content.classList.add("content");
-        square.appendChild(content);
-
-        grid.appendChild(square);
+    if (!Number.isInteger(newSize) || newSize <= 0) {
+        alert("Invalid size entered.");
+        return;
     }
+    createGrid(newSize);
+}
+
+function setup() {
+    createGrid(16);
 
     let clearButton = document.querySelector("#clear");
     clearButton.addEventListener("click", clearGrid);
+
+    let changeSizeButton = document.querySelector("#grid-size");
+    changeSizeButton.addEventListener("click", changeGridSize);
 }
 
 setup();
