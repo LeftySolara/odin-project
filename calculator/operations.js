b24e027ce5f2d3f1cecebe1bbc5418f7c0cb3ca8
Jalen Adams <jalen@jalenkadams.me>
2020-05-12 20:41:34 -0400

    Add: logic for performing calculations

--- calculator/operations.js
+++ calculator/operations.js
@@ -1,21 +1,125 @@
-function add(a, b) {
-    return a + b;
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    push(element) {
+        this.data.push(element);
+    }
+
+    pop() {
+        return this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+
+    isEmpty() {
+        return this.data.length === 0;
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
+function isOperator(ch) {
+    let operators = "^/*+-";
+    return operators.includes(ch);
 }
 
-function multiply(a, b) {
-    return a * b;
+function getPrecedence(ch) {
+    let precedence;
+    switch (ch) {
+        case "^":
+            precedence = 1;
+            break;
+        case "/":
+        case "*":
+            precedence = 2;
+            break;
+        case "+":
+        case "-":
+            precedence = 3;
+            break;
+        default:
+            precedence = -1;
+    }
+    return precedence;
 }
 
-function divide(a, b) {
-    return a / b;
+function process(operands, operators) {
+    let a = operands.pop();
+    let b = operands.pop();
+    let op = operators.pop();
+    let result;
+
+    switch (op) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = b - a;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = b / a;
+            break;
+        case "^":
+            result = a ** b;
+            break;
+    }
+    operands.push(result);
 }
 
-function operate(operator, a, b) {
-    return add(a, b);
+function evaluate(expression) {
+    let operands = new Stack();
+    let operators = new Stack();
+    let ch;
+
+    for (let i = 0; i < expression.length; ++i) {
+        ch = expression.charAt(i);
+
+        // Check if character is a number
+        if (!isNaN(ch)) {
+            // Keep parsing characters for multi-digit numbers
+            let num = "";
+            while (!isNaN(ch) || ch === ".") {
+                num = num + ch;
+                ++i;
+                if (i < expression.length) {
+                    ch = expression.charAt(i);
+                }
+                else {
+                    break;
+                }
+            }
+            --i;
+            // Convert string to number before pushing to stack
+            operands.push(+num);
+        }
+        else if (isOperator(ch)) {
+            while (!operators.isEmpty() && getPrecedence(ch) <= getPrecedence(operators.peek())) {
+                process(operands, operators);
+            }
+            operators.push(ch);
+        }
+        else if (ch === "(") {
+            operators.push(ch);
+        }
+        else if (ch === ")") {
+            // Closing brace, evaluate all enclosed operations
+            while (operators.peek() != "(") {
+                process(operands, operators);
+            }
+            operators.pop();
+        }
+    }
+
+    while (!operators.isEmpty()) {
+        process(operands, operators);
+    }
+    
+    return operands.pop();
 }
 
 function inputSymbol(number) {
@@ -48,6 +152,13 @@ function initialize() {
 
     let backspaceButton = document.querySelector("#backspace");
     backspaceButton.addEventListener("click", backspace);
+
+    let equalsButton = document.querySelector("#equals");
+    equalsButton.addEventListener("click", function(e) {
+        let display = document.querySelector("#display");
+        let result = evaluate(display.value);
+        display.value = result;
+    })
 
     clearDisplay();
 }
