00473f625d376430867dab5ab5274438ecd4303c
Jalen Adams <jalen@jalenkadams.me>
2020-05-23 10:39:35 -0400

    Add: functions to display books as cards

--- library/library.js
+++ library/library.js
@@ -8,18 +8,43 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function() {
-    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+    return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.read}`;
+}
+
+Book.prototype.createCard = function() {
+    let card = document.createElement("div");
+    card.classList.add("bookCard");
+
+    let cardLine;
+    let bookInfo = this.info().split(",");
+    bookInfo.forEach(line => {
+        cardLine = document.createElement("p");
+        cardLine.innerHTML = line.trim();
+        card.appendChild(cardLine);
+    });
+
+    return card;
 }
 
 function addBookToLibrary(book) {
     library.push(book);
 }
 
+function render() {
+    let libraryDiv = document.querySelector("#library");
+    let card;
+
+    library.forEach(book => {
+        card = book.createCard();
+        libraryDiv.appendChild(card);
+    });
+}
+
 // Sample data
-let book = new Book("The Pragmatic Programmer", "David Thomas", 300, true);
-let book2 = new Book("The Saga of Tanya the Evil, Vol. 1: Deus lo Vult", "Carlo Zen", 315, false);
+let book = new Book("The Pragmatic Programmer", "David Thomas", 300, "Yes");
+let book2 = new Book("The Saga of Tanya the Evil, Vol. 1: Deus lo Vult", "Carlo Zen", 315, "No");
 
 addBookToLibrary(book);
 addBookToLibrary(book2);
 
-console.log(library);
+render();
